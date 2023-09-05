f058b945f1db7b59f63e19462aeea0b3e1b188a5
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-09-04 18:33:57 -0700

    Add fallback for data fetching function

--- lib/user.ts
+++ lib/user.ts
@@ -7,7 +7,12 @@ export const getUser = async () => {
 };
 
 export const getUsers = async (): Promise<User[]> => {
-    const res = await fetch('http://127.0.0.1:5000/all-students');
-    const data = await res.json();
-    return data;
+    try {
+        const res = await fetch('http://127.0.0.1:5000/all-students');
+        const data = await res.json();
+        return data;
+    } catch (e) {
+        console.log('Could not fetch user data')
+        return []
+    }
 };
