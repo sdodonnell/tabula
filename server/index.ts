615cff1c61118d10c714d2ec7934cd9c1e12890d
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-10-14 16:15:59 -0700

    feat: add Prisma schema to /server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,26 @@
-console.log('hello')
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.submission.update({
+    where: { id: 1 },
+    data: { grade: 50, feedback: 'Just kidding, this is bad.' }
+  });
+  const allUsers = await prisma.user.findMany({
+    include: {
+      submittedAssignments: true
+    }
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
