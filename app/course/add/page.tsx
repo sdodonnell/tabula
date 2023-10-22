0bb6e13e5c578d1e5e53a4090c57e723182a6a49
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-10-21 21:13:13 -0700

    fix: make course edit form work

--- app/course/add/page.tsx
+++ app/course/add/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { CourseInputVariables, createCourse } from '@/lib/course/course';
-import { Form, Formik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 import { useRouter } from 'next/navigation';
 
 export default function NewUser() {
@@ -14,7 +14,7 @@ export default function NewUser() {
   return (
     <Formik
       initialValues={initialValues}
-      onSubmit={async (values, actions) => {
+      onSubmit={async (values) => {
         await createCourse(values);
         router.push('/course/all');
       }}
@@ -27,8 +27,7 @@ export default function NewUser() {
           >
             Name
           </label>
-          <input
-            type="text"
+          <Field
             id="name"
             name="name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -43,8 +42,7 @@ export default function NewUser() {
           >
             Term
           </label>
-          <input
-            type="text"
+          <Field
             id="term"
             name="term"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -59,10 +57,11 @@ export default function NewUser() {
           >
             Description
           </label>
-          <textarea
+          <Field
             id="description"
             name="description"
             rows={4}
+            as="textarea"
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Description goes here..."
           />
