1dac7abb3173f30c6d42226e6302f39ac11e6913
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-10-24 17:08:42 -0700

    fix: add missing env variable

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,1 @@
-export const GQL_ENDPOINT =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:4000/graphql'
-    : `https://${process.env.VERCEL_URL}/api/graphql`;
+export const GQL_ENDPOINT = 'http://localhost:4000/graphql';
