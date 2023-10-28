f8d5ee47af0d2d30387012cd0c4d92e8b4c8d03d
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-10-28 15:45:53 -0700

    feat: use user data for user button

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,6 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import { config } from '@/lib/auth';
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
-    })
-  ],
-});
+const handler = NextAuth(config);
 
 export { handler as GET, handler as POST };
--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 const nextConfig = {
-    experimental: {
-        serverActions: true
-    },
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'flowbite.s3.amazonaws.com',
-          },
-        ],
+  experimental: {
+    serverActions: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flowbite.s3.amazonaws.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      }
+    ],
+  },
 };
 
 module.exports = nextConfig;
