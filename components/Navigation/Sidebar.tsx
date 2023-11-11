e3aefa02b841b2e34a77f3aadd0ae5465f161083
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-11-11 14:59:27 -0800

    fix: fix type errors

--- components/Navigation/Sidebar.tsx
+++ components/Navigation/Sidebar.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 
+import { User } from '@/types';
+
 interface Props {
-  currentUser: UserSession;
+  currentUser: User;
 }
 
 export default async function Sidebar({ currentUser }: Props) {
