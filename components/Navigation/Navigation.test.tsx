fedf198dde6931b1f4c607380d7969e78aca2231
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-11-11 11:46:38 -0800

    feat: add testing library scaffolding

---
+++ components/Navigation/Navigation.test.tsx
@@ -0,0 +1,29 @@
+import NavBar from './NavBar';
+import Sidebar from './Sidebar';
+import UserMenu from './UserMenu';
+
+describe('NavBar', () => {
+  test('Renders app title', () => {
+    //
+  });
+
+  test('Renders menu links', () => {
+    //
+  });
+});
+
+describe('Sidebar', () => {
+  test('Shows correct links', () => {
+    //
+  });
+});
+
+describe('UserMenu', () => {
+  test('Menu opens when clicked', () => {
+    //
+  });
+
+  test("Displays current user's data", () => {
+    //
+  });
+});
